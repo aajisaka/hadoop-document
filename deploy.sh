a3a4dbdb7bf456f6f5845f10d25ac297b8177a46
Akira Ajisaka <aajisaka@apache.org>
2017-04-01 02:37:14 +0900

    Drop messages when pushing the document

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ git config --global user.email "aajisaka@apache.org"
 git config --global user.name "Akira Ajisaka"
 git add -A
 git commit -m "Automatic deploy to Github Pages"
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --quiet --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
