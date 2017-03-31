9463958b9c41926b8f60908cb9e685691ff6e066
Akira Ajisaka <aajisaka@apache.org>
2017-04-01 04:32:33 +0900

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/gh-pages
-git init
-git config --global user.email "aajisaka@apache.org"
-git config --global user.name "Akira Ajisaka"
-git add -A
-git commit -m "Automatic deploy to Github Pages"
-git push --quiet --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
