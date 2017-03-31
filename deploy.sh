574c28c2d11c032967d2ac139229ca8f7b2ecd8b
Akira Ajisaka <aajisaka@apache.org>
2017-04-01 05:04:14 +0900

    Enable logging in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cd ~/gh-pages
+
+set -x
 git init
 git config --global user.email "aajisaka@apache.org"
 git config --global user.name "Akira Ajisaka"
 git add -A
 git commit -m "Automatic deploy to Github Pages"
+
+# Avoid logging ${GH_TOKEN}
+set +x
 git push --quiet --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
